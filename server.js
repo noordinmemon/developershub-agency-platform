e542b9cfb80770d5a4b98ed264fe1c489a18ef9f
Nooruddin <noordinmemon@gmail.com>
2026-04-18 15:29:40 +0500

    Fix syntax error in app.listen

--- server.js
+++ server.js
@@ -144,5 +144,4 @@ mongoose.connect(process.env.MONGO_URI)
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
-    });
   .catch (err => console.log('MongoDB error:', err));
